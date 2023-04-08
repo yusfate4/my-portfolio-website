0fc4e2d778b1d8398f785be3b4c0ea1808492964
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-08 21:58:17 +0100

    added the about-me page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me_about.jpg";
+import { TbAwardFilled } from "react-icons/tb";
+import {HiOutlineUsers} from 'react-icons/hi'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About_Image" />
+          </div>
+        </div>
 
-export default About
+        <div className="about__content">
+          <div className="about__cards">
+            {/* card 1 */}
+            <article className="about__card">
+              <TbAwardFilled className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            {/* card 2 */}
+            <article className="about__card">
+              <HiOutlineUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            {/* card 3 */}
+            <article className="about__card">
+              <TbAwardFilled className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
