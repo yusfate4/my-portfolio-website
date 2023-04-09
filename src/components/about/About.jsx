eb799f0d7d525b656ec762569682f7119bf0945f
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-09 21:51:09 +0100

    Added the experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import "./about.css";
 import ME from "../../assets/me_about.jpg";
 import { TbAwardFilled } from "react-icons/tb";
-import {HiOutlineUsers} from 'react-icons/hi'
+import { HiOutlineUsers } from "react-icons/hi";
+import { RiFoldersLine } from "react-icons/ri";
 
 const About = () => {
   return (
@@ -34,11 +35,28 @@ const About = () => {
 
             {/* card 3 */}
             <article className="about__card">
-              <TbAwardFilled className="about__icon" />
+              <RiFoldersLine className="about__icon" />
               <h5>Projects</h5>
               <small>80+ Completed</small>
             </article>
           </div>
+
+          <p>
+            As a goal-oriented front-end developer, I am committed to building
+            strong, collaborative teams and providing the best solutions
+            possible. I am well-versed in both the front-end designs and
+            back-end logic necessary to create top-tier web experiences. I have
+            extensive experience in developing user-friendly interfaces that
+            both look and function great. I am well-versed in the latest
+            technologies, including HTML, CSS, JavaScript, Tailwind CSS,
+            BootStrap, and React and I am always looking to expand my knowledge
+            base. I am a motivated, goal-oriented individual with the knowledge and experience necessary
+            to build successful web experiences.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
         </div>
       </div>
     </section>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,20 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+
+      <div className="container experience__container">
+            <div className="experience__frontend">
+
+            </div>
+            <div className="experience__technical">
+              
+            </div>
+      </div>
+    </section>
   )
 }
 
