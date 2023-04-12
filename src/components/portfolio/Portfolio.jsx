95d916009641d30e46cfd2cac778ef40fc78ad73
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-12 21:20:23 +0100

    added the project section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+// portfolios project
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+  {
+    id: 2,
+    image: IMG3,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: "Crypto currency dashboard",
+    github: "https://github.com",
+    demo: "https://github.com/yusfate4",
+  },
+];
+
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+            <a href={github} className="btn" target="__blank">
+              Github
+            </a>
+            <a
+              href={demo}
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+            )
+          })
+        }
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
