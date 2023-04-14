f9ee7b6d7a958342acb73843ba6820c9493db9ee
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-04-14 21:32:20 +0100

    added the footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,115 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { FiTwitter } from "react-icons/fi";
+import { FaWhatsapp } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <section id='contact'>Contact</section>
-  )
-}
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-export default Contact
+    emailjs
+      .sendForm(
+        "service_4r11unu",
+        "template_85mbn3d",
+        form.current,
+        "0IHwBSYfcX4ePZs1c"
+      )
+
+
+
+
+
+
+
+      
+      // .then(
+      //   (result) => {
+      //     console.log(result.text);
+      //   },
+      //   (error) => {
+      //     console.log(error.text);
+      //   }
+      // );
+
+    // e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch </h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>abiodunyusuf4@gmail.com</h5>
+            <a
+              href="mailto:abiodunyusuf4@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <FiTwitter className="contact__option-icon" />
+            <h4>Twitter</h4>
+            <h5>Dahud Yusuf</h5>
+            <a
+              href="https://twitter.com/yusfate4"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            {/* <h5>+234</h5> */}
+            {/* https://api.whatsapp.com/send?phone=910000000000 */}
+            <a
+              href="https://api.whatsapp.com/send?phone=08174485504"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onClick={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your Full Name"
+            required
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Enter Your Email"
+            required
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Enter Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FiTwitter } from "react-icons/fi";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>{" "}
+        </li>
+        <li>
+          <a href="#about">About</a>{" "}
+        </li>
+        <li>
+          <a href="#experience">Experience</a>{" "}
+        </li>
+        <li>
+          <a href="#services">Services</a>{" "}
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>{" "}
+        </li>
+        <li>
+          <a href="#contact">Contact</a>{" "}
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com">
+          <FiTwitter />
+        </a>
+        <a href="https://linkedin.com">
+          <AiOutlineLinkedin />{" "}
+        </a>
+        <a href="https://github.com">
+          <AiOutlineGithub />
+        </a>
+      </div>
+
+
+      <div className="footer__copyright">
+        <small>&copy; Dahud Yusuf. </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
